63a06672a56d8270b371c9c24565f3f26d4e0232
Arves100 <arves100@gmail.com>
2021-04-03 21:48:53 +0200

    [UI] Add documentation in header

--- Pac85/Ui.h
+++ Pac85/Ui.h
@@ -9,25 +9,61 @@ enum EWindowID
 	WINDOWID_DetectMirror,
 };
 
+/*!
+ * Basic component of a Ui element, it must be implemented in every
+ * component of a window/dialog.
+ */
 struct SBasicComponent
 {
-	UINT nId;
-	HWND hWnd;
+	UINT nId; /*! ID of the component @see EWindowID */
+	HWND hWnd; /*! Handle of the component window */
 };
 
+/*!
+ * Contains all the elements of the Ui
+ */
 struct SUi
 {
-	HINSTANCE hInstance;
-	struct SBasicComponent* psActiveDlg;
-	struct SBasicComponent* psBackgroundWin;
+	HINSTANCE hInstance; /*! instance of this application */
+	struct SBasicComponent* psActiveDlg; /*! Current active dialog */
+	struct SBasicComponent* psBackgroundWin; /*! Main window/background component */
 };
 
+/*!
+ * Ui window loop
+ * @param psUi a pointer to an Ui structure
+ * @return exit code of the application
+ */
 extern INT UiLoop(_In_ struct SUi* psUi);
 
+/*!
+ * Switches the current dialog to another (or creates one if no dialog is active)
+ * @param psUi a pointer to an Ui structure
+ * @param nId the id of the dialog @see EWindowID
+ * @return result codes
+ */
 extern HRESULT UiSwitchToDialog(_In_ struct SUi* psUi, _In_ UINT nId);
+
+/*!
+ * Deletes the current active dialog
+ * @param psUi a pointer to an Ui structure
+ * @return result codes
+ */
 extern HRESULT UiDeleteActiveDialog(_In_ struct SUi* psUi);
 
+/*!
+ * Creates a new Ui structure
+ * @param hInstance instance of the application
+ * @param pOut output of the structure, if the function is not succedded pOut might be NULL or a garbage pointer
+ * @return result codes
+ */
 extern HRESULT UiCreate(_In_ HINSTANCE hInstance, _Out_ struct SUi** pOut);
+
+/*!
+ * Deletes and frees the component of an Ui structure
+ * @param psUi pointer to a structure to get deleted
+ * @return result codes
+ */
 extern HRESULT UiDelete(_In_ struct SUi* psUi);
 
 #endif /* PAC85_UI_H */
